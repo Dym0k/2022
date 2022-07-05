74c78e93bc2ce75b81cd80ab12cb6dad4230b828
Dym0k <90596851+Dym0k@users.noreply.github.com>
2022-07-05 11:53:09 +0300

    Add files via upload

---
+++ DK11/Tykhenko/lab5/main.cpp
@@ -0,0 +1,75 @@
+// Вар 5
+// Тихенко
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+
+class Text
+{
+protected:
+    const char *text;
+    long length;
+
+public:
+    void set_text(const char *new__text)
+    {
+        text = new__text;
+        length = strlen(text);
+    }
+
+    int get_text_length()
+    {
+        return length;
+    }
+
+    const char *get_all_text()
+    {
+        return text;
+    }
+
+    Text(const char *new__text)
+    {
+		text = new__text;
+		length = strlen(text);
+		display();
+    }
+
+    Text()
+    {
+        text = "";
+        length = 0;
+    }
+
+    void display()
+    {
+        cout << "\nText:\n"
+             << text << "\n";
+    }
+
+    ~Text()
+    {
+        cout << "Text destroyed\n";
+    }
+};
+
+int main(void)
+{
+    Text Text_one;
+    Text_one.set_text("Just text one");
+    Text_one.display();
+    cout << "Length: " << Text_one.get_text_length() << endl;
+    int index = 3;
+    cout << "\nGet all text:\n"
+         << Text_one.get_all_text() << "\n"
+         << endl;
+
+    Text Text_two("Text two");
+ 	cout << "Length: " << Text_two.get_text_length() << endl;
+    Text copy__Text_two = Text(Text_two);
+    copy__Text_two.display();
+
+    return 1;
+}
